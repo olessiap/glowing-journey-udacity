f12d16dadcad52aace639162f0b4e8a2eaac6e75
Olessia Potapova <olessia.potapova@gmail.com>
2016-10-31 19:15:11 -0700

    adding for loops

--- python_main.py
+++ python_main.py
@@ -539,3 +539,106 @@ print p
 #>>>[1, 2, [3, 4]]
 print len(p)
 #>>>3
+
+###FOR LOOPS ON LISTS = looping through the elements of a list ###
+
+def print_all_elements(p):
+    i = 0
+    while i < len(p):
+        print p[1]
+        i = i + 1
+#>>> prints out only the length of p
+
+def print_all_elements(p):
+    for e in p:
+        print e
+
+mylist = [1, 2, [3, 4]]
+print_all_elements(mylist)
+
+## for loops in lists practice
+
+print "EXAMPLE 1: We can use for loops to go through a list of strings"
+example_list_1 = ['a', 'b', 'c', 'd']
+for thing in example_list_1:
+    print thing
+#>>> abcd
+
+print "EXAMPLE 2: We can use for loops on nested lists too!"
+example_list_2 = [['China', 'Beijing'],
+                  ['USA'  , 'Washington D.C.'],
+                  ['India', 'Delhi']]
+for country_with_capital in example_list_2:
+    country = country_with_capital[0]
+    capital = country_with_capital[1]
+    print capital + ' is the capital of ' + country
+#>>>Beiking is the capital of China and etc
+
+# Define a procedure, sum_list,
+# that takes as its input a
+# list of numbers, and returns
+# the sum of all the elements in
+# the input list.
+
+def sum_list(p):
+    result = 0
+    for e in p:
+        result = result + e
+    return result
+
+print sum_list([1, 2, 3])
+#>>> 6
+
+def measure_udacity(U):
+    count = 0
+    for e in U:
+        if e[0] == 'U':
+            count = count + 1
+    return count
+
+print measure_udacity(['Dave', 'Ulia', 'Ugi', 'Uasss'])
+#>>> 3
+
+# Define a procedure, find_element,
+# that takes as its inputs a list
+# and a value of any type, and
+# returns the index of the first
+# element in the input list that
+# matches the value.
+
+# If there is no matching element,
+# return -1.
+
+####while loop answer ###
+def find_element(p,t):
+    i = 0
+    while i < len(p):
+        if p[i] == t:
+            return i
+        i = i + 1
+    return -1
+
+### FOR loop answer
+
+def find_element(p,t):
+    i = 0
+    for e in p:
+        if e == t:
+            return i
+        i = i + 1
+    return -1
+
+###INDEX answer ###
+
+def find_element (p, t):
+    if t in p:
+        return p.index(t)
+    else:
+        return -1
+
+### IN/NOT IN answer
+
+def find_element(p, t):
+    if t not in p:
+        return -1
+    return p.index(t)
