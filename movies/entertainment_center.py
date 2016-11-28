4f7709e9fde7b7edc768030cd41a790edd57faf2
Olessia Potapova <olessia@doubledutch.me>
2016-11-28 10:23:54 -0800

    movie problem start

---
+++ movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Ivan Vasilievich: Back to the Future",
+"Ivan the great travels to the future",
+"https://images-na.ssl-images-amazon.com/images/M/MV5BNDQ3NDM2NjM5MF5BMl5BanBnXkFtZTYwNTg1OTc5._V1_.jpg",
+"https://www.youtube.com/watch?v=nDWPKqXDNq8")
+
+print toy_story.storyline
---
+++ movies/media.py
@@ -0,0 +1,13 @@
+# class Movie
+    #   title
+    #   storyline
+    #   poster_image
+    #   youtube_trailer
+# show_trailer()
+
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
