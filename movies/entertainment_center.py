8ff86e68289ae730bc1c2c58b9c4ef5d2158e01d
Olessia Potapova <olessia.potapova@gmail.com>
2016-11-28 22:25:09 -0800

    added all 3 instances of the class Movie

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,8 +1,17 @@
 import media
 
-toy_story = media.Movie("Ivan Vasilievich: Back to the Future",
-"Ivan the great travels to the future",
-"https://images-na.ssl-images-amazon.com/images/M/MV5BNDQ3NDM2NjM5MF5BMl5BanBnXkFtZTYwNTg1OTc5._V1_.jpg",
-"https://www.youtube.com/watch?v=nDWPKqXDNq8")
+ivan_movie = media.Movie("Ivan Vasilievich: Back to the Future","Ivan the terrible travels to the future",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BNDQ3NDM2NjM5MF5BMl5BanBnXkFtZTYwNTg1OTc5._V1_.jpg",
+                         "https://www.youtube.com/watch?v=nDWPKqXDNq8")
 
-print toy_story.storyline
+moscow_movie = media.Movie("Moscow Does Not Believe in Tears", "Story about a Moscow woman and her rise in society",
+                           "https://images-na.ssl-images-amazon.com/images/I/51-6zxtejTL.jpg",
+                           "https://www.youtube.com/watch?v=69ellPkReh0")
+
+fate_movie = media.Movie("The Irony of Fate or Enjoy Your Bath", "A man accidently ends up in the wrong apartment on NYE and changes the fate of 2 people",
+                         "https://www.podnapisi.net/thumbnails/moviedb/normal/VDc.jpg",
+                         "https://www.youtube.com/watch?v=nNrFdBvBYWg")
+#print toy_story.storyline
+#print moscow_movie.storyline
+
+fate_movie.show_trailer()
--- movies/media.py
+++ movies/media.py
@@ -4,6 +4,7 @@
     #   poster_image
     #   youtube_trailer
 # show_trailer()
+import webbrowser
 
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -11,3 +12,6 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
