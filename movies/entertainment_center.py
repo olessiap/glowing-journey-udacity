6d84333855ce8ed777e09b143749c6810f610848
Olessia Potapova <olessia.potapova@gmail.com>
2016-12-23 16:42:42 -0800

    added 6 movies

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -11,7 +11,22 @@ moscow_movie = media.Movie("Moscow Does Not Believe in Tears", "Story about a Mo
 fate_movie = media.Movie("The Irony of Fate or Enjoy Your Bath", "A man accidently ends up in the wrong apartment on NYE and changes the fate of 2 people",
                          "https://www.podnapisi.net/thumbnails/moviedb/normal/VDc.jpg",
                          "https://www.youtube.com/watch?v=nNrFdBvBYWg")
+
+brother_movie = media.Movie("Brother", "2 brothers turn into thugs",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BNDMxNzkwNzkwNV5BMl5BanBnXkFtZTcwNTczNDkxMQ@@._V1_.jpg",
+                         "https://www.youtube.com/watch?v=Zq55yxODJiA")
+
+nineth_company_movie = media.Movie("9th company", "Russian boys become soldiers in the Afghan war & become bffs in tragic circumstances",
+                         "https://i.jeded.com/i/the-9th-company-9-ya-rota.19239.jpg",
+                         "https://www.youtube.com/watch?v=TcHD9VEQ7C8")
+
+nightwatch_movie = media.Movie("Nightwatch", "Supernatural beings fight to protect their realms",
+                         "https://en.wikipedia.org/wiki/File:Night_Watch_(2004_film)_theatrical_poster.jpg",
+                         "https://www.youtube.com/watch?v=xDPLZW8MBW8")
+
+
+
 #print toy_story.storyline
 #print moscow_movie.storyline
 
-fate_movie.show_trailer()
+nightwatch_movie.show_trailer()
